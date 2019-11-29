a0db0d49292b6595796df3a332a6fd0d22895772
beanbubger <gotablast99@gmail.com>
2019-11-28 21:19:11 -0600

    Connect to cluster

--- svc/mongo_setup.py
+++ svc/mongo_setup.py
@@ -1,4 +1,7 @@
 import mongoengine
+import os
+
+host = os.environ.get('HOST')
 
 def global_init():
-    mongoengine.register_connection(alias="core", name="Johnson", host="localhost")
+    mongoengine.register_connection(alias="core", name="Johnson", host=host)

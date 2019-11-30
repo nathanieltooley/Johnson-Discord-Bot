a81e642661e366057ff300685271e8a64d89b404
beanbubger <gotablast99@gmail.com>
2019-11-30 15:22:32 -0600

    Add reload command

--- coggers.py
+++ coggers.py
@@ -14,6 +14,11 @@ async def load(ctx, extension):
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f"cogs.{extension}")
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
     
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):

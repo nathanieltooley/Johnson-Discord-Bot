f1b28f8fd370644b2af6fab8f9c8256d3ba662e0
beanbubger <gotablast99@gmail.com>
2019-12-02 17:04:54 -0600

    Add more values to BaseItem

--- svc/items.py
+++ svc/items.py
@@ -1,14 +1,19 @@
 import mongoengine
 import uuid
 
+choices = ("Common", "Rare", "Epic")
+
 class BaseItem(mongoengine.Document):
     _id = mongoengine.UUIDField(default=uuid.uuid4())
     name = mongoengine.StringField(required=True)
+    description = mongoengine.StringField()
     value = mongoengine.IntField(required=True)
+    rarity = mongoengine.StringField(default="Common", choices=choices)
 
     meta: {
         "allow_inheritance": True,
-        "db_alias": "core"
+        "db_alias": "core",
+        "collection": "Items"
     }
 
 class Item(mongoengine.EmbeddedDocument):

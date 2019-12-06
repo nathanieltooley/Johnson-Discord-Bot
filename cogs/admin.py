a48bf273501713685c6d5367b36ac02f92c39625
beanbubger <gotablast99@gmail.com>
2019-12-05 22:47:45 -0600

    Update admin

--- cogs/admin.py
+++ cogs/admin.py
@@ -28,36 +28,6 @@ class Admin(commands.Cog):
     async def spawn_item(self, ctx, member: discord.Member, ref_id):
         item, value = svc.give_item_to_user(member, ref_id, ctx.guild)
         await ctx.send(f"Given User Item: {item}, of Value: {value} V-Bucks")
-
-    @commands.has_permissions(administrator=True)
-    @commands.command()
-    async def get_user_inventory(self, ctx, member: discord.Member = None):
-        if member is None:
-            item = svc.get_user_inventory(ctx.author, ctx.guild)
-            name = ctx.author.nick
-        else:
-            item = svc.get_user_inventory(member, ctx.guild)
-            if member.bot:
-                name = member.name
-            else:
-                name = member.nick
-
-        count = len(item)
-
-        embed = discord.Embed(
-            title=f"{name}'s Inventory",
-            description=f"The inventory of {name}. Total Item Count {count}",
-            color=discord.Colour.dark_purple()
-        )
-
-        if isinstance(item, list):
-            item = enumerate(item, 1)
-            for count, i in item:
-                embed.add_field(name=f"{i.name}", value=f"{i.value} V-Bucks", inline=False)
-
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send(item)
     
 
 def setup(client):

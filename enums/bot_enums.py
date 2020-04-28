09f66a03655ccbc27265f5e6a97dbecd2168da8c
beanbubger <gotablast99@gmail.com>
2020-04-28 01:42:29 -0500

    Change avatar pic

--- enums/bot_enums.py
+++ enums/bot_enums.py
@@ -2,4 +2,4 @@ from enum import Enum
 
 
 class Bot(Enum):
-    BOT_AVATAR_URL = "https://cdn.discordapp.com/attachments/610520962898853908/610539291701149709/AntiHa.jpg"
+    BOT_AVATAR_URL = "https://cdn.discordapp.com/attachments/649780106885070868/649786020870750251/JOHNSON.png"

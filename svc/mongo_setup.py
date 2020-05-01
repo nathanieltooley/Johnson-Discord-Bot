4379c828e0eebe40c8adc9b0e879c3f1e6793fdb
beanbubger <gotablast99@gmail.com>
2020-05-01 03:10:34 -0500

    Add auto-reconnect

--- svc/mongo_setup.py
+++ svc/mongo_setup.py
@@ -1,7 +1,14 @@
 import mongoengine
 import os
+import pymongo
 
 host = os.environ.get('HOST')
 
+
 def global_init():
-    mongoengine.register_connection(alias="default", name="Johnson", host=host)
+    try:
+        mongoengine.register_connection(alias="default", name="Johnson", host=host)
+        print("Connected!")
+    except pymongo.errors.ConfigurationError:
+        print("Could not connect; Retrying...")
+        global_init()

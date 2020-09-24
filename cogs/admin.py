7fb2939e9822c28e619a233ee021f10ee49ca85a
Nathaniel Tooley <nathanieltooley24@gmail.com>
2020-09-24 00:53:50 -0500

    Added talk channel command like for real this time

--- cogs/admin.py
+++ cogs/admin.py
@@ -41,6 +41,12 @@ class Admin(commands.Cog):
     async def talk(self, ctx, message):
         channel = discord.utils.find(lambda x: x.id == 649780790468542516, ctx.guild.text_channels)
         await channel.send(message)
+
+    @commands.has_permissions(administrator=True)
+    @commands.command()
+    async def talk_ch(self, ctx, message, channel_name: str):
+        channel = discord.utils.find(lambda x: channel_name.lower() in x.name.lower(), ctx.guild.text_channels)
+        await channel.send(message)
         
     
 

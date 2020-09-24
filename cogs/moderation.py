9229e53bff4aed1993af2809519324b9228e45a1
Nathaniel Tooley <nathanieltooley24@gmail.com>
2020-09-24 16:14:28 -0500

    Creates role if no mute role found

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -19,12 +19,23 @@ class Moderation(commands.Cog):
     @commands.has_permissions(administrator=True)
     @commands.command()
     async def mute(self, ctx, member: discord.Member, seconds=0, reason="Just shut up"):
-        await ctx.send(f"{member.mention} muted!")
+        mute_role = None
+        check_mute = discord.utils.find(lambda x: x.name == "Muted", ctx.guild.roles)
 
+        if check_mute:
+            mute_role = check_mute
+        else:
+            mute_perms = discord.Permissions.text()
+            mute_perms.send_messages = False
+            mute_role = await ctx.guild.create_role(name="Muted", permissions=mute_perms, color=discord.Color.red(),
+                                                    reason="Missing Mute Role")
+
+        utils.Logging.log(__name__, f"print: {mute_role}")
         user_roles = member.roles
-        mute_role = ctx.guild.get_role(758732926333747210)
 
-        await member.add_roles(mute_role, reason=f"{reason}; for {seconds} seconds")
+        await member.add_roles(mute_role, reason=f"{reason}; for {seconds} second(s)")
+
+        await ctx.send(f"{member.mention} muted!")
 
         # Skip the first role (@everyone)
         for i in range(1, len(user_roles)):

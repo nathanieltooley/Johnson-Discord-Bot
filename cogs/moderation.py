64063efd7a2f35da63474837f2db9b3b24c07c63
Nathaniel Tooley <nathanieltooley24@gmail.com>
2020-09-24 01:00:52 -0500

    Added moderation cog and test command

---
+++ cogs/moderation.py
@@ -0,0 +1,16 @@
+import discord
+
+from discord.ext import commands, tasks
+
+class Moderation(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.has_permissions(administrator=True)
+    @commands.command()
+    async def test_mod(self, ctx):
+        await ctx.send("Yep it works")
+
+def setup(client):
+    client.add_cog(Moderation(client))

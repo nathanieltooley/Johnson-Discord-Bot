35adadb85fe6fff031962f02758cc1486749d492
Nathaniel Tooley <nathanieltooley24@gmail.com>
2022-01-08 23:35:23 -0600

    Added environment variable check

--- coggers.py
+++ coggers.py
@@ -10,11 +10,26 @@ intents.members = True
 
 client = commands.Bot(intents=intents, command_prefix=".")
 slash = SlashCommand(client, sync_commands=True, sync_on_cog_reload=True)
-       
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
-        client.load_extension(f'cogs.{filename[:-3]}') # Cut off .py
-        utils.Logging.log("coggers", f"{filename} loaded")
 
-client.run(os.environ.get("TOKEN"))
+if __name__ == '__main__':
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            client.load_extension(f'cogs.{filename[:-3]}')  # Cut off .py
+            utils.Logging.log("coggers", f"{filename} loaded")
+
+    environ_vars = ["DISCORD_HOST", "HOST", "LEVEL", "SPOTIPY_CLIENT_ID", "SPOTIPY_CLIENT_SECRET",
+                    "SPOTIPY_REDIRECT_URI", "TOKEN"]
+
+    ev_not_set = False
+    for var in environ_vars:
+        if os.getenv(var, None) is None:
+            utils.Logging.error(__name__, f"Environment Variable, {var}, is not set")
+            ev_not_set = True
+
+    if ev_not_set:
+        exit()
+
+    client.run(os.environ.get("TOKEN"))
+
+
 

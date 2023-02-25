e53ff5449c4b8644147171e6794c3b9ef1426a33
nathanieltooley <gotablast99@gmail.com>
2023-02-25 00:57:52 -0600

    added song classes

--- data_models/music.py
+++ data_models/music.py
@@ -1,43 +1,52 @@
+import discord
+
 from enums.bot_enums import ReturnTypes as return_types
 from utils import jspotify, youtube
 from abc import ABC, abstractmethod
+from typing import List
 
-class QueuedSong:
+class BaseSong(ABC):
 
-    def __init__(self, url, url_type, title="", authors=None):
+    def __init__(self, url: str) -> None:
         self.url = url
-        self.url_type = url_type
-        self.title = title
-        self.authors = authors
 
-        if not title == "" and authors is not None:
-            self.props_set = True
-        else:
-            self.props_set = False
+    @abstractmethod
+    def get_song_properties(self) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def color(self) -> discord.Color:
+        raise NotImplementedError
+    
+class YDLSong(BaseSong):
 
-    def cache_properties(self):
-        if self.url_type == return_types.RETURN_TYPE_SPOTIFY_URL:
-            track_info = jspotify.get_track(jspotify.parse_id_out_of_url(self.url))
+    def get_song_properties(self) -> None:
+        info = youtube.get_video_info(self.url)
 
-            self.title = track_info["name"]
-            self.authors = jspotify.get_artist_names(track_info)
+        self.title = info["title"]
 
-            self.props_set = True
-        else:
-            info = youtube.get_video_info(self.url)
+        # I put them in a tuple because otherwise the author string function will count each character as an author
+        # ex. If the uploader's name is Johnson then it would be displayed "J, o, h, n, s, o, n" if not in a tuple
+        try:
+            self.authors = (info["creator"],)
+        except KeyError:
+            self.authors = (info["uploader"],)
 
-            self.title = info["title"]
+    def color(self) -> discord.Color:
+        return discord.Color.red()
+    
+class SoundcloudSong(YDLSong):
+    
+    def color(self) -> discord.Color:
+        return discord.Color.orange()
+    
+class SpotifySong(BaseSong):
+    
+    def get_song_properties(self) -> None:
+        track_info = jspotify.get_track(jspotify.parse_id_out_of_url(self.url))
 
-            # I put them in a tuple because otherwise the author string function will count each character as an author
-            # ex. If the uploader's name is Johnson then it would be displayed "J, o, h, n, s, o, n" if not in a tuple
-            try:
-                self.authors = (info["creator"],)
-            except KeyError:
-                self.authors = (info["uploader"],)
+        self.title = track_info["name"]
+        self.authors = jspotify.get_artist_names(track_info)
 
-            self.props_set = True
-
-    def clear_props(self):
-        self.title = ""
-        self.authors = None
-        self.props_set = False
+    def color(self) -> discord.Color:
+        return discord.Color.green()

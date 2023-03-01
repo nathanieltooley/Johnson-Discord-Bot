6476f7cc7b515e7bd9232a42c2e260ea2fad6dbc
nathanieltooley <gotablast99@gmail.com>
2023-02-28 18:50:41 -0600

    fixed logging color bug

--- utils/jlogging.py
+++ utils/jlogging.py
@@ -2,17 +2,17 @@ import colorama
 import datetime
 
 def log(name, log):
-    print(colorama.Style.RESET_ALL + colorama.Fore.BLUE + f"{name}" + colorama.Fore.MAGENTA + f":{log}")
+    print(colorama.Style.RESET_ALL + colorama.Fore.BLUE + f"{name}" + colorama.Fore.MAGENTA + f":{log}" + colorama.Style.RESET_ALL)
 
 
 def time_log(name, log):
     print(colorama.Style.RESET_ALL + colorama.Fore.BLUE + f"{name}" + colorama.Fore.GREEN +
-            f"{datetime.datetime.now()}" + colorama.Fore.MAGENTA + f":{log}")
+            f"{datetime.datetime.now()}" + colorama.Fore.MAGENTA + f":{log}" + colorama.Style.RESET_ALL)
 
 
 def error(name, log):
-    print(colorama.Fore.RED + f"ERROR:{name}:{log}")
+    print(colorama.Fore.RED + f"ERROR:{name}:{log}" + colorama.Style.RESET_ALL)
 
 
 def warning(name, log):
-    print(colorama.Fore.YELLOW + f"WARNING:{name}:{log}")
+    print(colorama.Fore.YELLOW + f"WARNING:{name}:{log}" + colorama.Style.RESET_ALL)

5ca93524d57541131a55f44553f516aaca6a66aa
nathanieltooley <gotablast99@gmail.com>
2023-11-08 22:51:24 -0600

    Added real logging methods

--- utils/jlogging.py
+++ utils/jlogging.py
@@ -1,5 +1,15 @@
+import sys
 import colorama
 import datetime
+import logging
+import pytz
+
+
+class JohnsonFormatter(logging.Formatter):
+    def converter(self, timestamp):
+        dt = datetime.datetime.fromtimestamp(timestamp)
+        tzinfo = pytz.timezone("US/Central")
+        return tzinfo.localize(dt)
 
 
 def log(name, log):
@@ -32,3 +42,22 @@ def error(name, log):
 
 def warning(name, log):
     print(colorama.Fore.YELLOW + f"WARNING:{name}:{log}" + colorama.Style.RESET_ALL)
+
+
+def get_logger(name, bot_level) -> logging.Logger:
+    log_level = logging.DEBUG
+    if bot_level == "PROD":
+        log_level = logging.INFO
+
+    jl = logging.getLogger(name)
+    jl.setLevel(log_level)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(get_formatter())
+    jl.addHandler(handler)
+
+    return jl
+
+
+def get_formatter() -> logging.Formatter:
+    return JohnsonFormatter("%(acstime)s - %(levelname)s - %(message)s")

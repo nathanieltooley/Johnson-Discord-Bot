f09a1bf79446608b65333e784fd4c598afd69e61
nathanieltooley <gotablast99@gmail.com>
2023-11-09 19:17:45 -0600

    Split formatters to allow for color logging

--- utils/jlogging.py
+++ utils/jlogging.py
@@ -4,8 +4,15 @@ import datetime
 import logging
 import pytz
 
+from typing import Mapping, Union
 
-class JohnsonFormatter(logging.Formatter):
+
+# Im using two different formatters because of some quirks with logging formatters
+# I tried manually setting the self._fmt attribute then ran super().format()
+# but it did not seem to keep the modified format so I ended up spliting them
+class CentralTimeFormatter(logging.Formatter):
+    """Formats the current time in US/Central to an ISO format"""
+
     def converter(self, timestamp):
         dt = datetime.datetime.fromtimestamp(timestamp)
         tzinfo = pytz.timezone("US/Central")
@@ -24,36 +31,38 @@ class JohnsonFormatter(logging.Formatter):
         return s
 
 
-def log(name, log):
-    print(
-        colorama.Style.RESET_ALL
-        + colorama.Fore.BLUE
-        + f"{name}"
-        + colorama.Fore.MAGENTA
-        + f":{log}"
-        + colorama.Style.RESET_ALL
-    )
+class ColorFormatter(logging.Formatter):
+    """
+    Formats the message to use colors using colorama.
+    Also uses the CentralTimeFormatter to format the time
+    """
 
+    def __init__(
+        self,
+        fmt=None,
+        datefmt=None,
+        style="%",
+        validate: bool = True,
+    ) -> None:
+        self._level_colors = {
+            logging.DEBUG: colorama.Style.DIM + colorama.Fore.BLUE,
+            logging.INFO: colorama.Fore.BLUE,
+            logging.WARN: colorama.Fore.YELLOW,
+            logging.ERROR: colorama.Style.BRIGHT + colorama.Fore.RED,
+            logging.CRITICAL: colorama.Style.BRIGHT + colorama.Fore.RED,
+        }
+        super().__init__(fmt, datefmt, style, validate)
 
-def time_log(name, log):
-    print(
-        colorama.Style.RESET_ALL
-        + colorama.Fore.BLUE
-        + f"{name}"
-        + colorama.Fore.GREEN
-        + f"{datetime.datetime.now()}"
-        + colorama.Fore.MAGENTA
-        + f":{log}"
-        + colorama.Style.RESET_ALL
-    )
+    def format(self, record: logging.LogRecord) -> str:
+        f = (
+            colorama.Style.RESET_ALL
+            + f"%(asctime)s - {self._level_colors.get(record.levelno)}%(levelname)s - %(message)s"
+            + colorama.Style.RESET_ALL
+        )
 
+        temp_f = CentralTimeFormatter(f, self.datefmt)
 
-def error(name, log):
-    print(colorama.Fore.RED + f"ERROR:{name}:{log}" + colorama.Style.RESET_ALL)
-
-
-def warning(name, log):
-    print(colorama.Fore.YELLOW + f"WARNING:{name}:{log}" + colorama.Style.RESET_ALL)
+        return temp_f.format(record)
 
 
 def get_logger(name, bot_level) -> logging.Logger:
@@ -72,4 +81,4 @@ def get_logger(name, bot_level) -> logging.Logger:
 
 
 def get_formatter() -> logging.Formatter:
-    return JohnsonFormatter("%(asctime)s - %(levelname)s - %(message)s")
+    return ColorFormatter(datefmt="%Y/%m/%d - %H:%M:%S")
